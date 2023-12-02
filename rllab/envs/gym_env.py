66b85dcdcaac5b55b292a461b83bfd510d93dbd1
SijingYu <51205048+SijingYu@users.noreply.github.com>
2023-12-02 14:38:05 -0500

    Update gym_env.py

--- rllab/envs/gym_env.py
+++ rllab/envs/gym_env.py
@@ -8,7 +8,7 @@ import logging
 try:
     # from gym.wrappers.monitoring import logger as monitor_logger
     # import gym.wrappers.logger as monitor_logger
-    import gym.logger as monitor_logger
+    from gym import logger as monitor_logger
     monitor_logger.setLevel(logging.WARNING)
 except Exception as e:
     traceback.print_exc()
